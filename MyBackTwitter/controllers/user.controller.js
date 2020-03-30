e696cd15c6b1f072fb03f5fa13e03158d068c296
NaslyGordi <46800532+NaslyGordi@users.noreply.github.com>
2020-03-29 19:52:56 -0500

    user.controller.js
    user
    
    Co-Authored-By: naslygordi <naslygordi@users.noreply.github.com>

--- MyBackTwitter/controllers/user.controller.js
+++ MyBackTwitter/controllers/user.controller.js
@@ -6,11 +6,11 @@ const dbManager = require ('../database.config/db.manager');
  */
 async function createUser (req, res) {    
     console.log(req.body);
-    if (!req.body) {// verifica si el contenido esta vacio
+    if (!req.body) {// CHECK IF THE REQUEST BODY IS EMPTY
         res.status(400).send({ message: "Request body is empty!!!!" });
         return;
     }
-    const newUserObject = {// creacion de objeto
+    const newUserObject = {// CREATING THE OBJECT TO PERSIST
         username: req.body.username,
         creation_date: req.body.creation_date
     }
@@ -18,11 +18,95 @@ async function createUser (req, res) {
         data => { res.send (data); }
     ).catch (
         e => {
-            console.log(e);// imprimir error en la consola
-            res.status(500).send({ message: "*******rror occurred********" });// mensaje de error
+            console.log(e);// Print error on console
+            res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
         }
     );
 }
+/**
+ * GEt all users
+ */
+async function findAllUsers (req, res){
+    try {
+        const users = await dbManager.User.findAll ();//Execute query
+        res.json({ data: users });//Send response
+    } catch (e) {
+        console.log(e);// Print error on console
+        res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
+    }
+}
+/**
+ * Get user by id
+ */
+async function findOneUser (req, res){
+    try {
+        const { idUser } = req.params;//Execute query
+        const user = await dbManager.User.findOne({ where: { idUser: idUser } });
+        res.json(user);//Send response
+    } catch (e) {
+        console.log(e);// Print error on console
+        res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
+    }
+}
+/**
+ * Update user
+ */
+async function updateUser (req, res){
+    if (!req.body) {// CHECK IF THE REQUEST BODY IS EMPTY
+        res.status(400).send({ message: "Request body is empty!!!!" });
+        return;
+    }
+    const newUserObject = {// CREATING THE OBJECT TO PERSIST
+        username: req.body.username,
+        creation_date: req.body.creation_date
+    }
+    const { idUser } = req.params;//Execute query
+    dbManager.User.update(newUserObject, { where: { idUser: idUser } }).then (// EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+        data => { res.send (newUserObject); }
+    ).catch (
+        e => {
+            console.log(e);// Print error on console
+            res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
+        }
+    );
+}
+/**
+ * Delete an existen user by username
+ * @param {*} req 
+ * @param {*} res 
+ */
+function deleteUserByUsername (req, res){ 
+    const { username } = req.params;//Execute query
+    dbManager.User.destroy( { where: { username: username } })// EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+        //data => { res.send (data); }
+    .catch (
+        e => {
+            console.log(e);// Print error on console
+            res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
+        }
+    );
+}
+/**
+ * @param {*} req 
+ * @param {*} res 
+ */
+function deleteAllUsers (req, res){
+    dbManager.User.destroy( { where: {} } )// EXECUTING THE DESTROY QUERY - INSERT THE OBJECT INTO DATABASE 
+    .catch (
+        e => {
+            console.log(e);// Print error on console
+            res.status(500).send({ message: "Some error occurred" });// Send error message as a response 
+        }
+    );
+}
+/** 
+ * @param {*} req 
+ * @param {*} res 
+ */
 
-
-exports.createUser = createUser;
+exports.createUser = createUser; 
+exports.findAllUsers = findAllUsers; 
+exports.findOneUser = findOneUser; 
+exports.updateUser = updateUser;
+exports.deleteUserByUsername = deleteUserByUsername;
+exports.deleteAllUsers = deleteAllUsers;
